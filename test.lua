1ad665483d605c4bdf6f156105b8c237975bfcfe
David <dblackstone107@gmail.com>
2024-05-27 22:02:34 -0400

    t

--- test.lua
+++ test.lua
@@ -1,8 +1,8 @@
 local component = require("component")
 local gpu = component.gpu
 local width, height = gpu.getResolution()
-while true do
-    gpu.setForeground(0xFF00FF)
-    gpu.setBackground(0x00A6FF)
-    gpu.fill((width / 2) / (10/2), (height / 2) / (50/2), 50, 10, "▄")
-end
+gpu.setBackground(0x000000)
+gpu.fill(0, 0, width, height,"▄")
+gpu.setForeground(0xFF00FF)
+gpu.setBackground(0x00A6FF)
+gpu.fill(width / 2, height / 2, 10, 50, "▄")
