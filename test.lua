bf0b144b22589db246794959b04c74bd0be05e23
David <dblackstone107@gmail.com>
2024-05-27 21:43:45 -0400

    tesating drawq

--- test.lua
+++ test.lua
@@ -1,1 +1,9 @@
-print("hello GTNH")
+local colors = require("colors")
+local component = require("component")
+local gpu = component.gpu
+
+while true do
+    gpu.setForeground(colors.gray)
+    gpu.setBackground(colors.orange)
+    gpu.fill(1, 1, 10, 10, colors.white)
+end
