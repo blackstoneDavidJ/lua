15aa5ce17fd14cb43360956cf26112a72383eb63
David <dblackstone107@gmail.com>
2024-05-27 21:45:47 -0400

    t2

--- test.lua
+++ test.lua
@@ -1,9 +1,8 @@
-local colors = require("colors")
-local component = require("component")
+local colors = require "colors"
+local component = require "component"
 local gpu = component.gpu
 
-while true do
-    gpu.setForeground(colors.gray)
-    gpu.setBackground(colors.orange)
-    gpu.fill(1, 1, 10, 10, colors.white)
-end
+gpu.setForeground(colors.gray)
+gpu.setBackground(colors.orange)
+
+gpu.fill(1, 1, 10, 10, colors.white)
