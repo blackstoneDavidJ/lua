2eeeaa31e5188cbbc664d1795f434ff2be1215d2
David <dblackstone107@gmail.com>
2024-05-27 21:50:35 -0400

    hex

--- test.lua
+++ test.lua
@@ -1,8 +1,6 @@
-local colors = require("colors")
 local component = require("component")
 local gpu = component.gpu
 
-gpu.setForeground(colors.gray.tostring)
-gpu.setBackground(colors.orange.tostring)
-
+gpu.setForeground(0xFF00FF)
+gpu.setBackground(0x00A6FF)
 gpu.fill(1, 1, 10, 10, "+")
