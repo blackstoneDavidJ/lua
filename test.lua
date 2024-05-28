fd6c9f9c084913503ba54a1e56df8d7b3f34bdd4
David <dblackstone107@gmail.com>
2024-05-27 22:16:17 -0400

    www

--- test.lua
+++ test.lua
@@ -4,8 +4,8 @@ local width, height = gpu.getResolution()
 
 gpu.setBackground(0x00A6FF)
 local size = 50
-local x = width / 2
-local y = height / 2
+local x = math.floor((width - size) / 2)
+local y = math.floor((height - size) / 2)
 for i = 0, size - 1 do
-    gpu.fill(x, y + i, size, 1, "▄")
+    gpu.fill(x, y + i, size, 1, " ")
 end
